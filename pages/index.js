d72d9d83f13c20b8a8c4f9c37e3747dd6a42d5c1
crunchymoon <sldenys96@hotmail.com>
2022-05-19 00:07:15 +0200

    feat(Home.module.scss, global.scss): bg img fixing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.scss'
 const currentLink = 'https://rickandmortyapi.com/api/character/'
+import mypic from '../public/bgRickk.jpg'
+import Image from 'next/image'
+
+
 
 export async function getServerSideProps() {
   const res = await fetch(currentLink);
@@ -10,11 +14,13 @@ export async function getServerSideProps() {
 
 export default function Home({ data }) {
 
+  console.log(mypic)
+
   const [currentData, setCurrentData] = useState(data);
   const [currentChar, setcurrentChar] = useState(currentData.results)
   console.log(currentChar);
   return (
-    <>
+    <div className={styles.mainContainer}>
       <ul className={styles.cardContainer}>
         {
           currentChar.map((character) => {
@@ -27,9 +33,15 @@ export default function Home({ data }) {
             )
           })
         }
+ {/* <Image
+      src={mypic}
+      alt="Picture of the author"
+      layout='fill'
+      objectFit='cover'
+      objectPosition='center'
+    /> */}
 
       </ul>
-    
-    </>
+    </div>
   )
 }
