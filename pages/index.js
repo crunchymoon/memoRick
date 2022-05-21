e01616714b48f39c2f05762869f88dd2a0f87521
crunchymoon <sldenys96@hotmail.com>
2022-05-21 09:46:24 +0200

    feat(index.js): all characters can be fetched and displayed in allChar variable.

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,50 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
-  // const [img,setImg] = useState('');
+
   const [currentData, setCurrentData] = useState(data);
-  const [currentChar, setcurrentChar] = useState(currentData.results)
+  const [currentChar, setCurrentChar] = useState(currentData.results)
+  // const [allChars, setAllchars] = useState(currentData.results);
+  const [allChars,setAllchars] = useState([...currentChar]);
+  
+  useEffect(() => {
+    if (currentData.info.next==null){
+      return
+    } else {
+      
+        async function getAllCharacters() {
+          const res = await fetch(currentData.info.next);
+          const data = await res.json();
+          setCurrentData(data)
+          // setAllchars(data.results)
+          
+          // console.log(allChars)
+          const circurr = [...currentChar,...data.results]
+          setAllchars(circurr)
+  
+        }
+        getAllCharacters()
+      
+    }
+  }, [currentData])
+    
+
+
+  
+
   return (
     <div className={styles.mainContainer}>
+      <p>{allChars.map((allChar)=>{
+        console.log(allChar)
+      })}</p>
       <ul className={styles.cardContainer}>
         {
           currentChar.map((character) => {
             const { id, name, image } = character;
             return (
               <li key={id} className={styles.card} >
-                <img className={styles.cardImg} onClick={((e)=>{e.target.src=image})} src={'/backRick.jpg'} alt={`${name}'s Thumb`}>
+                {/* <img className={styles.cardImg} onClick={((e) => { e.target.src = image })} src={'/backRick.jpg'} alt={`${name}'s Thumb`}> */}
+                <img className={styles.cardImg} src={image} alt={`${name}'s Thumb`}>
                 </img>
               </li>
             )
