69c42cb7e984e2c71e65dfcbe89422fc7db0bee9
crunchymoon <sldenys96@hotmail.com>
2022-05-21 10:31:04 +0200

    feat(index.js): added a function that randomly chooses a character ad updates the current character , next: fixing prop bugs..

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,14 @@ import Image from 'next/image'
 
 //useState : img --> bakcRickk, onClick data.image
 
+function getMultipleRandom(arr, num) {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+  return shuffled.slice(0, num);
+}
+
+
+
 export async function getServerSideProps() {
   const res = await fetch(currentLink);
   const data = await res.json();
@@ -16,45 +24,40 @@ export default function Home({ data }) {
 
   const [currentData, setCurrentData] = useState(data);
   const [currentChar, setCurrentChar] = useState(currentData.results)
-  // const [allChars, setAllchars] = useState(currentData.results);
-  const [allChars,setAllchars] = useState([...currentChar]);
-  
+  const [allChars, setAllchars] = useState([...currentChar]);
+
   useEffect(() => {
-    if (currentData.info.next==null){
+    if (currentData.info.next == null) {
       return
     } else {
-      
-        async function getAllCharacters() {
-          const res = await fetch(currentData.info.next);
-          const data = await res.json();
-          setCurrentData(data)
-          // setAllchars(data.results)
-          
-          // console.log(allChars)
-          const circurr = [...currentChar,...data.results]
-          setAllchars(circurr)
-  
-        }
-        getAllCharacters()
-      
+
+      async function getAllCharacters() {
+        const res = await fetch(currentData.info.next);
+        const data = await res.json();
+        setCurrentData(data)
+
+        const circurr = [...currentChar, ...data.results]
+        setAllchars(circurr)
+        // setCurrentChar(allChars) 
+        
+
+      }
+      getAllCharacters()
+
     }
   }, [currentData])
-    
-
-
-  
+  const random5 = getMultipleRandom(allChars, 5)
 
   return (
     <div className={styles.mainContainer}>
-      <p>{allChars.map((allChar)=>{
-        console.log(allChar)
-      })}</p>
       <ul className={styles.cardContainer}>
         {
-          currentChar.map((character) => {
+          
+          random5.map((character) => {
+            // console.log(character)
             const { id, name, image } = character;
             return (
-              <li key={id} className={styles.card} >
+              <li key={id} className={styles.card} onClick={(()=>{console.log(id)})}>
                 {/* <img className={styles.cardImg} onClick={((e) => { e.target.src = image })} src={'/backRick.jpg'} alt={`${name}'s Thumb`}> */}
                 <img className={styles.cardImg} src={image} alt={`${name}'s Thumb`}>
                 </img>
