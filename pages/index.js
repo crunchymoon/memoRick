596c7a8b889855e68883461053555c7dbc6945c4
crunchymoon <sldenys96@hotmail.com>
2022-05-22 15:03:45 +0200

    feat(index.js, Home.module.scss): copy of the same random characters with random orders

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home({ data }) {
   const [currentChar, setCurrentChar] = useState(currentData.results)
   const [allChars, setAllchars] = useState([...currentChar]);
   const [randomChars, setRandomChars] = useState([])
+  const [copyChars, setCopyChars] = useState([])
   const [update, setUpdate] = useState(false)
 
   const getMultipleRandom = (ar, size) => {
@@ -24,15 +25,18 @@ export default function Home({ data }) {
     return ar.length <= (size + 1) ? new_ar : getMultipleRandom(new_ar, size);
   }
 
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included 
+  }
+
   useEffect(() => {
     if (currentData.info.next == null) {
-      setTimeout(()=>{
-        setUpdate(true);
-      },1800)
-      
-
+      // setTimeout(()=>{
+      setUpdate(true);
+      // },1800)
     } else {
-
       async function getAllCharacters() {
         const res = await fetch(currentData.info.next);
         const data = await res.json();
@@ -40,18 +44,19 @@ export default function Home({ data }) {
         const circurr = [...currentChar, ...data.results]
         setAllchars(circurr)
         setCurrentChar(allChars)
-
       }
       getAllCharacters()
-
     }
   }, [currentData])
 
 
   useEffect(() => {
-    setRandomChars(getMultipleRandom(allChars, 20))
-  }, [update])
+    setRandomChars(getMultipleRandom(allChars, 10))
 
+  }, [update])
+  useEffect(() => {
+    setCopyChars(randomChars)
+  }, [randomChars])
 
   return (
     <div className={styles.mainContainer}>
@@ -61,7 +66,7 @@ export default function Home({ data }) {
             randomChars.map((character) => {
               const { id, name, image } = character;
               return (
-                <li key={id} className={styles.card} onClick={(() => { console.log(id) })}>
+                <li style={{ order: getRandomIntInclusive(0,100) }} key={id} className={`${styles.card} `} onClick={(() => { console.log(id) })}>
                   {/* <img className={styles.cardImg} onClick={((e) => { e.target.src = image })} src={'/backRick.jpg'} alt={`${name}'s Thumb`}> */}
                   <img className={styles.cardImg} src={image} alt={`${name}'s Thumb`}>
                   </img>
@@ -70,7 +75,21 @@ export default function Home({ data }) {
             })
           )
         }
-        {!update && <Skeleton type='loading'/>}
+        {
+          update && (
+            copyChars.map((character) => {
+              const { id, name, image } = character;
+              return (
+                <li style={{ order: getRandomIntInclusive(0,100) }} key={id} className={styles.card} onClick={(() => { console.log(id) })}>
+                  {/* <img className={styles.cardImg} onClick={((e) => { e.target.src = image })} src={'/backRick.jpg'} alt={`${name}'s Thumb`}> */}
+                  <img className={styles.cardImg} src={image} alt={`${name}'s Thumb`}>
+                  </img>
+                </li>
+              )
+            })
+          )
+        }
+        {!update && <Skeleton type='loading' />}
 
       </ul>
     </div>
